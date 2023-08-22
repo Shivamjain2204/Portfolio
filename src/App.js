489d1e0e1be586ea046ba653b1c400418404baa5
Shivam <jainshivam2204@gmail.com>
2023-08-23 00:32:59 +0530

    Home and About Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import About from './components/about/About';
 import Header from './components/header/Header';
+import Home from './components/home/Home';
 
 function App() {
   return (
     <div className="App">
     <Header/>
+
+    <main className='main'>
+      <Home/>
+      <About/>
+    </main>
     </div>
   );
 }
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://www.linkedin.com/in/shivam-jain2204/" className="home__social-icon" targrt="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a href="https://twitter.com/" className="home__social-icon" targrt="_blank">
+        <i class="uil uil-twitter-alt"></i>
+        </a>
+
+        <a href="https://github.com/Shivamjain2204" className="home__social-icon" targrt="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+
+export default Social
