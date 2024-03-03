eb59c75549e8302f817580cd589cb771cd43dd44
Shivam <jainshivam2204@gmail.com>
2024-03-03 14:52:47 +0530

    Work data updated

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,53 +1,90 @@
-import Work1 from "../../assets/work1.jpg"
-import Work2 from "../../assets/work2.jpg"
-import Work3 from "../../assets/work3.jpg"
-import Work4 from "../../assets/work4.jpg"
-import Work5 from "../../assets/work5.jpg"
+import Work1 from "../../assets/CodeSphere.jpg"
+import Work2 from "../../assets/BlogApp.jpg"
+import Work3 from "../../assets/Notepad.jpg"
+import Work4 from "../../assets/MusicApp.jpg"
+import Work5 from "../../assets/Crypto.jpg"
+import Work6 from "../../assets/WeatherApp.jpg"
+import Work7 from "../../assets/MessageBoard.jpg"
+import Work8 from "../../assets/SnakeGame.jpg"
+import Work9 from "../../assets/FlipkartClone.jpg"
 
 export const projectData = [
     {
         id: 1,
         image: Work1,
-        title: 'Web design',
-        category: 'web',
+        title: 'CodeSphere',
+        category: '1',
+        link: "https://nextjs-project-theta-taupe.vercel.app/"
     },
     {
         id: 2,
         image: Work2,
-        title: 'Web design',
-        category: 'web',
+        title: 'BlogApp',
+        category: '1',
+        link: "https://myblog-murex-two.vercel.app/"
     },
     {
         id: 3,
         image: Work3,
-        title: 'Web design',
-        category: 'design',
+        title: 'Notepad',
+        category: '1',
+        link: "https://notepad-delta-five.vercel.app/login"
     },
     {
         id: 4,
         image: Work4,
-        title: 'Web design',
-        category: 'app',
+        title: 'Music App',
+        category: '2',
+        link: "https://music-clone-tau.vercel.app/"
     },
     {
         id: 5,
         image: Work5,
-        title: 'Web design',
-        category: 'web',
+        title: 'Crypto-currency',
+        category: '2',
+        link: "https://github.com/Shivamjain2204/crypto-currency"
+    },
+    {
+        id: 6,
+        image: Work6,
+        title: 'Weather Channel',
+        category: '2',
+        link: "https://weather-app-peach-beta.vercel.app/"
+    },
+    {
+        id: 7,
+        image: Work7,
+        title: 'Message Board',
+        category: '3',
+        link: "https://message-board-weld.vercel.app/"
+    },
+    {
+        id: 8,
+        image: Work8,
+        title: 'Snake Game',
+        category: '3',
+        link: "https://snake-game-topaz.vercel.app/"
+    },
+    {
+        id: 9,
+        image: Work9,
+        title: 'Flipkart Clone',
+        category: '3',
+        link: "https://flipkart-clone-sigma.vercel.app/"
     },
 ]
 
 export const projectsNav = [
+    // {
+    //     name: 'all'
+    // },
     {
-        name: 'all'
+        name: '1'
     },
     {
-        name: 'app'
+        name: '2'
     },
     {
-        name: 'web'
-    },
-    {
-        name: 'design'
+        name: '3'
     },
 ]
